4f0af59429b3261fe05556faf0d99165a9536ce6
lisboamab <lisboamab@gmail.com>
2022-06-01 03:01:16 -0300

    Diferentes formas de concatenar com Dart

---
+++ fundamentos/area.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+main(){
+  const double PI = 3.1415;
+  stdout.write("Informe o raio: ");
+  final entradaDoUsuario = stdin.readLineSync()!;
+  final double raio = double.parse(entradaDoUsuario);
+
+  final area = PI * raio * raio;
+
+  print("Para um valor de raio de $raio á área é de: $area");
+}
+
